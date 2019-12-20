5b5134b63830c30cfdb7e314e3090a1f4ff5ab57
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2019-12-20 07:13:49 +0530

    printing command line data continuously

--- alphabets.js
+++ alphabets.js
@@ -1,27 +1,38 @@
 const { stdout } = process;
-const { alphabetDimensions } = require("./dimensions");
+const alphabetDimensions = require("./dimensions");
 
 const printAt = (str, x, y) => {
 	stdout.cursorTo(x, y);
 	stdout.write(str);
 };
 
-const printGivenAlphabet = function(dimensions) {
-	if (dimensions.length == 0) return;
-	let position = dimensions.shift();
-	printAt("*", position[0], position[1] - 10);
-	printGivenAlphabet(dimensions);
-};
-
-const printAlphabets = function() {
-	const dimensions = alphabetDimensions.shift();
+const printGivenAlphabet = (dimensions, interval) => {
+	console.clear();
 	if (!dimensions) {
 		clearInterval(interval);
 		return;
 	}
-	console.clear();
-	printGivenAlphabet(dimensions);
+	dimensions.forEach(position => {
+		printAt("*", position[0], position[1]);
+	});
 };
 
-console.clear();
-const interval = setInterval(printAlphabets, 1000);
+const printAlphabets = function(alphabets) {
+	let requiredDimensions = [];
+	alphabets.forEach(alphabet => {
+		const dimensions = alphabetDimensions[alphabet];
+		requiredDimensions.push(dimensions);
+	});
+	const interval = setInterval(
+		() => printGivenAlphabet(requiredDimensions.shift(), interval),
+		1000
+	);
+};
+
+const main = () => {
+	const name = process.argv[2];
+	const alphabets = name.toUpperCase().split("");
+
+	printAlphabets(alphabets);
+};
+main();
